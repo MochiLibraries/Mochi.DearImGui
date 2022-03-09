c724f847b2abe7c505c51f7424a9e9fcfdaa387a
David Maas <contact@pathogenstudios.com>
2022-03-09 06:05:58 -0600

    Removed stack-allocated UTF16 -> UTF8 string conversion wrapper to make things easier to read.

--- Mochi.DearImGui.Generator/#Declarations/ImGuiStringFunctionWrapperDeclaration.cs
+++ Mochi.DearImGui.Generator/#Declarations/ImGuiStringFunctionWrapperDeclaration.cs
@@ -1,5 +1,4 @@
-﻿#define USE_INTERPOLATED_STRING_HANDLER
-using Biohazrd;
+﻿using Biohazrd;
 using Biohazrd.CSharp;
 using Biohazrd.CSharp.Infrastructure;
 using Biohazrd.Transformation;
@@ -51,23 +50,13 @@ namespace Mochi.DearImGui.Generator
             }
 
             // Automated naming conventions
-#if USE_INTERPOLATED_STRING_HANDLER
             static string GetParameterBufferName(TranslatedParameter parameter)
                 => SanitizeIdentifier($"__{parameter.Name}");
-#endif
 
             static string GetParameterPointerName(TranslatedParameter parameter)
                 => SanitizeIdentifier($"__{parameter.Name}P");
 
-#if !USE_INTERPOLATED_STRING_HANDLER
-            const string temporaryBufferName = "__temporaryBuffer";
-            const string pinnedTemporaryBufferName = $"{temporaryBufferName}P";
-            const string temporaryBufferLengthName = $"{temporaryBufferName}Length";
-            const string encodedLengthName = "__encodedLength";
-            const int maximumStackAllocationSize = 2048;
-#else
             const string temporaryReturnValueName = "__result";
-#endif
 
             // Write out the function signature
             VisitorContext functionContext = context.MakePrevious().Add(function);
@@ -76,17 +65,8 @@ namespace Mochi.DearImGui.Generator
             writer.EnsureSeparation();
             writer.Using("System"); // Span<T>, ReadOnlySpan<T>
             writer.Using("System.Diagnostics"); // DebuggerStepThroughAttribute, DebuggerHiddenAttribute
-#if !USE_INTERPOLATED_STRING_HANDLER
-            writer.Using("System.Text"); // Encoding
-#else
             writer.Using("Mochi.DearImGui"); // DearImGuiInterpolatedStringHandler
             const string interpolatedStringHandlerType = "DearImGuiInterpolatedStringHandler";
-#endif
-
-            // Note: We do not use aggressive inlining here to avoid allocating stack in our caller if the JIT isn't a fan of the idea.
-#if !USE_INTERPOLATED_STRING_HANDLER // These are causing stepping into these calls to be unecessarily confusing.
-            writer.WriteLine("[DebuggerStepThrough, DebuggerHidden]");
-#endif
 
             string? constructorName;
             if (function.SpecialFunctionKind == SpecialFunctionKind.Constructor && context.ParentDeclaration is TranslatedRecord constructorType)
@@ -126,16 +106,8 @@ namespace Mochi.DearImGui.Generator
 
                         if (!forArguments)
                         {
-#if USE_INTERPOLATED_STRING_HANDLER
                             writer.Write($"{interpolatedStringHandlerType} {SanitizeIdentifier(parameter.Name)}");
-#else
-                            writer.Write($"ReadOnlySpan<char> {SanitizeIdentifier(parameter.Name)}");
-#endif
                         }
-#if !USE_INTERPOLATED_STRING_HANDLER
-                        else if (isFirstStringParameter)
-                        { writer.Write(pinnedTemporaryBufferName); }
-#endif
                         else
                         { writer.Write(GetParameterPointerName(parameter)); }
 
@@ -144,11 +116,7 @@ namespace Mochi.DearImGui.Generator
                         {
                             if (forArguments)
                             {
-#if USE_INTERPOLATED_STRING_HANDLER
                                 writer.Write($", {GetParameterPointerName(parameter)} + {GetParameterBufferName(parameter)}.Length");
-#else
-                                writer.Write($", {GetParameterPointerName(function.Parameters[i + 1])}");
-#endif
                             }
 
                             i++;
@@ -200,7 +168,6 @@ namespace Mochi.DearImGui.Generator
 
             using (writer.Block())
             {
-#if USE_INTERPOLATED_STRING_HANDLER
                 // Allocate temporary for the return value if necessary
                 if (function.ReturnType is not VoidTypeReference)
                 { writer.WriteLine($"{outputGenerator.GetTypeAsString(functionContext, function, function.ReturnType)} {temporaryReturnValueName};"); }
@@ -252,84 +219,6 @@ namespace Mochi.DearImGui.Generator
                     writer.WriteLine();
                     writer.WriteLine($"return {temporaryReturnValueName};");
                 }
-#else
-                // Allocate a temporary buffer for performing the UTF16 -> UTF8 conversion
-                writer.Write($"int {temporaryBufferLengthName} = ");
-                writer.Write(sortedStringParameterIndices.Length); // Extra bytes for null terminators
-                foreach (int i in sortedStringParameterIndices)
-                { writer.Write($" + Encoding.UTF8.GetMaxByteCount({SanitizeIdentifier(function.Parameters[i].Name)}.Length)"); }
-                writer.WriteLine(';');
-
-                writer.WriteLine
-                (
-                    $"Span<byte> {temporaryBufferName} = {temporaryBufferLengthName} <= {maximumStackAllocationSize} ? " +
-                    $"stackalloc byte[{temporaryBufferLengthName}] : " +
-                    $"new byte[{temporaryBufferLengthName}];"
-                );
-
-                writer.WriteLine($"int {encodedLengthName};");
-
-                // Pin the temporary buffer
-                // There's no sense in trying to avoid having the buffer pinned during encoding because the implementation of UTF8Encoding.GetBytes will pin it anyway
-                // https://github.com/dotnet/runtime/blob/4c92aef2b08f9c4374c520e7e664a44f1ad8ce56/src/libraries/System.Private.CoreLib/src/System/Text/UTF8Encoding.cs#L373-L382
-                writer.WriteLine();
-                writer.WriteLine($"fixed (byte* {pinnedTemporaryBufferName} = {temporaryBufferName})");
-                using (writer.Block())
-                {
-                    // Perform the conversions
-                    for (int ii = 0; ii < sortedStringParameterIndices.Length; ii++)
-                    {
-                        TranslatedParameter parameter = function.Parameters[sortedStringParameterIndices[ii]];
-                        TranslatedParameter? endParameter = ii == 0 && ParameterFollowingFirstIsStringEnd ? function.Parameters[sortedStringParameterIndices[ii] + 1] : null;
-
-                        // Do the conversion
-                        writer.WriteLine($"{encodedLengthName} = Encoding.UTF8.GetBytes({SanitizeIdentifier(parameter.Name)}, {temporaryBufferName});");
-                        writer.WriteLine($"{temporaryBufferName}[{encodedLengthName}] = 0;"); // Add null terminator
-
-                        // If this parameter has an end parameter, create that end parameter
-                        if (endParameter is not null)
-                        {
-                            writer.Write($"byte* {GetParameterPointerName(endParameter)} = ");
-
-                            if (ii == 0)
-                            { writer.Write(pinnedTemporaryBufferName); }
-                            else
-                            { writer.Write(GetParameterPointerName(parameter)); }
-
-                            writer.WriteLine($" + {encodedLengthName};");
-                        }
-
-                        // Update the output buffer and save a pointer for the next parameter
-                        if ((ii + 1) < sortedStringParameterIndices.Length)
-                        {
-                            TranslatedParameter nextParameter = function.Parameters[sortedStringParameterIndices[ii + 1]];
-
-                            writer.WriteLine($"{encodedLengthName}++;");
-                            writer.WriteLine();
-                            writer.WriteLine($"{temporaryBufferName} = {temporaryBufferName}.Slice({encodedLengthName});");
-
-                            writer.Write($"byte* {GetParameterPointerName(nextParameter)} = ");
-
-                            if (ii == 0)
-                            { writer.Write(pinnedTemporaryBufferName); }
-                            else
-                            { writer.Write(GetParameterPointerName(parameter)); }
-
-                            writer.WriteLine($" + {encodedLengthName};");
-                        }
-                    }
-
-                    // Write out the function call
-                    writer.WriteLine();
-                    if (function.ReturnType is not VoidTypeReference)
-                    { writer.Write("return "); }
-
-                    writer.WriteIdentifier(function.Name);
-                    writer.Write('(');
-                    WriteOutParameters(forArguments: true);
-                    writer.WriteLine(");");
-                }
-#endif
             }
         }
     }
